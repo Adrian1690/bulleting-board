40ac7b98b211943462fd5f0338715725b104e157
Adrian Pena Romero <adrian.pena@avantica.net>
2018-02-02 17:28:56 -0500

    first commit

---
+++ src/Note.js
@@ -0,0 +1,65 @@
+/*
+ * Module dependecias
+ */
+import React from 'react'
+import FaPencil from 'react-icons/lib/fa/pencil'
+import FaTrash from 'react-icons/lib/fa/trash'
+import FaFloppyO from 'react-icons/lib/fa/floppy-o'
+
+class Note extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            editing : false
+        }
+        this.edit = this.edit.bind(this)
+        this.remove = this.remove.bind(this)
+        this.save = this.save.bind(this)
+        this.renderForm = this.renderForm.bind(this)
+        this.renderDisplay = this.renderDisplay.bind(this)
+    }
+
+    edit(){
+        this.setState({
+            editing: true
+        })
+    }
+
+    remove(){
+        alert('removing')
+    }
+
+    save(){
+        alert('saving')
+    }
+
+    renderForm(){
+        return (
+            <div className="note">
+                <form>
+                    <textarea />
+                    <button onClick={this.save} ><FaFloppyO /></button>
+                </form>
+            </div>
+        )
+    }
+
+    renderDisplay(){
+        return (
+            <div className="note">
+                <p>Learn React</p>
+                <span>
+                    <button onClick={this.edit} id="edit"><FaPencil /> </button>
+                    <button onClick={this.remove} id="remove"><FaTrash /> </button>
+                </span>
+            </div>   
+        )
+    }
+
+    render(){
+        return this.state.editing ? this.renderForm() : this.renderDisplay()
+    }
+}
+ 
+export default Note;
