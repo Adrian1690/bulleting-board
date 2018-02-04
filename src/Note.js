29c1bd457aa2bbfff54e70700832e2508b756592
Adrian Peña <j.adrian.1690@gmail.com>
2018-02-04 17:28:55 -0500

    finishing

--- src/Note.js
+++ src/Note.js
@@ -18,6 +18,34 @@ class Note extends React.Component {
         this.save = this.save.bind(this)
         this.renderForm = this.renderForm.bind(this)
         this.renderDisplay = this.renderDisplay.bind(this)
+        this.randomBetween = this.randomBetween.bind(this)
+    }
+
+    componentWillMount(){
+        this.style = {
+            right : this.randomBetween(0, window.innerWidth - 150, 'px'),
+            top : this.randomBetween(0, window.innerHeight - 150, 'px'),
+            transform : `rotate(${this.randomBetween(-25, 25, 'deg')})`
+        }
+    }
+
+    componentDidUpdate(){
+        var textArea
+        if(this.state.editing) {
+            textArea = this._newText
+            textArea.focus()
+            textArea.select()
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        return(
+            this.props.children !== nextProps.children || this.state !== nextState
+        )
+    }
+
+    randomBetween(x, y, s){
+        return x + Math.ceil(Math.random() * (y-x)) + s
     }
 
     edit(){
@@ -27,19 +55,25 @@ class Note extends React.Component {
     }
 
     remove(){
-        alert('removing')
+        this.props.onRemove(this.props.index)
     }
 
-    save(){
-        alert('saving')
+    save(e){
+        e.preventDefault();
+        this.props.onChange(this._newText.value, this.props.index)
+        this.setState({
+            editing: false
+        })
     }
 
     renderForm(){
         return (
-            <div className="note">
-                <form>
-                    <textarea />
-                    <button onClick={this.save} ><FaFloppyO /></button>
+            <div className="note" style={this.style}>
+                <form onSubmit={this.save}>
+                    <textarea   ref={input => this._newText = input}
+                                defaultValue={this.props.children}
+                                />
+                    <button id="add"><FaFloppyO /></button>
                 </form>
             </div>
         )
@@ -47,8 +81,8 @@ class Note extends React.Component {
 
     renderDisplay(){
         return (
-            <div className="note">
-                <p>Learn React</p>
+            <div className="note" style={this.style}>
+                <p>{this.props.children}</p>
                 <span>
                     <button onClick={this.edit} id="edit"><FaPencil /> </button>
                     <button onClick={this.remove} id="remove"><FaTrash /> </button>
